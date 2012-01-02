69439479a9b6cdce68e01eb29b0fd472dd3b7bcf
Julian Lettner <julian.lettner@gmail.com>
2012-01-02 22:40:16 +0100

    Add class Slot and tests

---
+++ YlnLib.Test/SlotTest.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using FluentAssertions;
+using Nukito;
+
+namespace YlnLib.Test
+{
+  public class SlotTest
+  {
+    [NukitoFact]
+    public void ShouldThrowForEmptySlotWithoutDefault()
+    {
+      // Arrange
+      var slot = new Slot<string>();
+
+      // Act
+      string item = null;
+      Action action = () => item = slot.Item;
+
+      // Assert
+      slot.HasItem.Should().BeFalse();
+      slot.HasDefault.Should().BeFalse();
+      slot.CanGet.Should().BeFalse();
+
+      action.ShouldThrow<InvalidOperationException>()
+        .And.Message.Should().Be("Item not set");
+      item.Should().BeNull();
+    }
+
+    [NukitoFact]
+    public void ShouldNotThrowForEmptySlotWithDefault()
+    {
+      // Arrange
+      var slot = new Slot<string>(defaultIfItemNotSet: "def");
+
+      // Act
+      string item = null;
+      Action action = () => item = slot.Item;
+
+      // Assert
+      slot.HasItem.Should().BeFalse();
+      slot.HasDefault.Should().BeTrue();
+      slot.CanGet.Should().BeTrue();
+
+      action.ShouldNotThrow();
+      item.Should().Be("def");
+    }
+
+    [NukitoFact]
+    public void ShouldNotThrowForStandardUsageWithoutDefault()
+    {
+      // Arrange
+      var slot = new Slot<string>();
+
+      // Act
+      slot.Set("abc");
+      string item = null;
+      Action action = () => item = slot.Item;
+
+      // Assert
+      slot.HasItem.Should().BeTrue();
+      slot.HasDefault.Should().BeFalse();
+      slot.CanGet.Should().BeTrue();
+
+      action.ShouldNotThrow();
+      item.Should().Be("abc");
+    }
+
+    [NukitoFact]
+    public void ShouldNotThrowForStandardUsageWithDefaultAndReturnItem()
+    {
+      // Arrange
+      var slot = new Slot<string>(defaultIfItemNotSet: "def");
+
+      // Act
+      slot.Set("abc");
+      string item = null;
+      Action action = () => item = slot.Item;
+
+      // Assert
+      slot.HasItem.Should().BeTrue();
+      slot.HasDefault.Should().BeTrue();
+      slot.CanGet.Should().BeTrue();
+
+      action.ShouldNotThrow();
+      item.Should().Be("abc");
+    }
+
+    [NukitoFact]
+    public void ShouldThrowForSecondSet()
+    {
+      // Arrange
+      var slot = new Slot<string>();
+
+      // Act
+      slot.Set("first");
+      Action action = () => slot.Set("second");
+
+      // Assert
+      slot.HasItem.Should().BeTrue();
+
+      action.ShouldThrow<InvalidOperationException>()
+        .And.Message.Should().Be("Item already set");
+    }
+
+    [NukitoFact]
+    public void ShouldUseProvidedItemNameForExceptionMessages()
+    {
+      // Arrange
+      var slot = new Slot<string>(itemName: "blub");
+
+      // Act
+      string item = null;
+      Action getWithNoItem = () => item = slot.Item;
+      Action setTooOften = () => slot.Set("second");
+
+      // Assert
+      getWithNoItem.ShouldThrow<InvalidOperationException>()
+        .And.Message.Should().Be("blub not set");
+
+      slot.Set("first");
+
+      setTooOften.ShouldThrow<InvalidOperationException>()
+        .And.Message.Should().Be("blub already set");
+    }
+  }
+}
---
+++ YlnLib/Slot.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace YlnLib
+{
+  public class Slot<T>
+  {
+    private readonly string _itemName;
+    private T _item;
+
+    public Slot(string itemName = "Item")
+    {
+      _itemName = itemName;
+    }
+
+    public Slot(T defaultIfItemNotSet, string itemName = "Item")
+      : this(itemName)
+    {
+      _item = defaultIfItemNotSet;
+      HasDefault = true;
+    }
+
+    public bool HasItem { get; private set; }
+    public bool HasDefault { get; private set; }
+
+    public bool CanGet
+    {
+      get { return HasItem || HasDefault; }
+    }
+
+    public T Item
+    {
+      get
+      {
+        if (!CanGet)
+          throw new InvalidOperationException(_itemName + " not set");
+
+        return _item;
+      }
+    }
+
+    public void Set(T item)
+    {
+      if (HasItem)
+        throw new InvalidOperationException(_itemName + " already set");
+
+      _item = item;
+      HasItem = true;
+    }
+  }
+}

f0b7e7936a51ccdc18ff940937ce2be9c38ef1c5
Julian Lettner <julian.lettner@gmail.com>
2012-01-04 00:39:56 +0100

    Add En.Sure and tests

---
+++ YlnLib.Test/EnsureTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using FluentAssertions;
+using Nukito;
+
+namespace YlnLib.Test
+{
+  public class EnsureTest
+  {
+    [NukitoFact]
+    public void ShouldNotThrowForTrueCondition()
+    {
+      // Act
+      Action action = () => En.Sure(true, null);
+
+      // Assert
+      action.ShouldNotThrow();
+    }
+
+    [NukitoFact]
+    public void ShouldThrowForFalseCondition()
+    {
+      // Act
+      Action action = () => En.Sure(false, "message");
+
+      // Assert
+      action.ShouldThrow<InvalidOperationException>();
+    }
+
+    [NukitoFact]
+    public void ShouldThrowWithFormattedExceptionMessage()
+    {
+      // Act
+      Action action = () => En.Sure(false, "invalid {0}. type {1}", 2, typeof (void));
+
+      // Assert
+      action.ShouldThrow<InvalidOperationException>()
+        .And.Message.Should().Be("invalid 2. type System.Void");
+    }
+  }
+}
---
+++ YlnLib/Ensure.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace YlnLib
+{
+  public static class En
+  {
+    public static void Sure(bool condition, string messageFormat, params object[] messageArguments)
+    {
+      if (!condition)
+        throw new InvalidOperationException(string.Format(messageFormat, messageArguments));
+    }
+  }
+}

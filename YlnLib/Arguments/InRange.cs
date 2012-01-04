153c2b844181d653e0723cfe6cd9781fe70e8999
Julian Lettner <julian.lettner@gmail.com>
2012-01-04 16:52:21 +0100

    Add InRange

---
+++ YlnLib/Arguments/InRange.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace YlnLib.Arguments
+{
+  public static partial class ArgumentHelper
+  {
+    public static int InRange(this int argument, int minInclusive, int maxExclusive, string argumentName = "value")
+    {
+      if (argument < minInclusive || argument >= maxExclusive)
+        throw new ArgumentOutOfRangeException(argumentName, argument,
+                                              string.Format("Range: [{0},{1})", minInclusive, maxExclusive));
+
+      return argument;
+    }
+  }
+}
